8da63357ea4f95f26a659b7a25ef81d5e261aa3e
shawn chollette <boywondercreative@gmail.com>
2014-03-14 00:53:19 -0500

    add files

---
+++ templates/add_hero.php
@@ -0,0 +1,9 @@
+<div class="hero">
+    <div class="container">
+    <div class="feature-bg container"></div> 
+        <div class="hero-container">
+            <?php the_post_thumbnail(9999,9999, false);?>
+        </div>
+		<div class="feature-wrapper container"></div> 
+    </div> 
+</div>
---
+++ templates/add_togglenav.php
@@ -0,0 +1,7 @@
+<div id="slide-nav" class="navbar navbar-default navbar-fixed-top visible-xs">
+    <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-recalc="false" data-target=".navmenu" data-canvas=".canvas">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+</div>
