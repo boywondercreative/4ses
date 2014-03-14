a75353387160cbc2649e759b30d2f129ff6e9caf
shawn chollette <boywondercreative@gmail.com>
2014-03-14 15:29:54 -0500

    main-wrap has a drop shadow ... duh!

--- templates/add_togglenav.php
+++ templates/add_togglenav.php
@@ -2,7 +2,7 @@
 $children = get_pages('child_of='.$post->ID);
 if( count( $children ) != 0 ) {  
 if (is_page()) { ?> 
-    <div class="main-wrap clearfix">
+    <div class="slide-nav-wrap clearfix">
         <div id="slide-nav" class="navbar navbar-default visible-xs">
             <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-recalc="false" data-target=".navmenu" data-canvas=".canvas">
               <span class="icon-bar"></span>
